90873d3e5f499cc167e6253251f87610647e7e0c
tili21 <tili08001@utb.vaxjo.se>
2025-11-04 12:37:07 +0100

    Add files via upload

--- Program (1).cs
+++ Program (1).cs
@@ -1,19 +1,23 @@
 ﻿using System;
 
-namespace BlueScreen
+namespace Uppgift_2_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Clear();
+            Console.Write("Hur långt hoppade Elin (meter)? ");
+            int elin = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Ett allvarligt systemfel har inträffat.");
-            Console.WriteLine("Datorn måste startas om.");
+            Console.Write("Hur långt hoppade Alma (meter)? ");
+            int alma = int.Parse(Console.ReadLine());
 
-       
+            int skillnad = elin - alma;
+
+            Console.WriteLine($"Elin hoppade {skillnad} meter längre än Alma.");
+
+            
+            Console.ReadKey();
         }
     }
 }
