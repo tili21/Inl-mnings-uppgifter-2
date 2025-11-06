a546f3ef61503a240a7f0e24aad4f8be930ce2fd
tili21 <tili08001@utb.vaxjo.se>
2025-11-06 20:53:37 +0100

    Add files via upload

--- Program (2).cs
+++ Program (2).cs
@@ -1,15 +1,22 @@
 ﻿using System;
 
-namespace Triangel
+namespace Uppgift_2_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("    /\\\"");
-            Console.WriteLine("   /  \\\"");
-            Console.WriteLine("  / \"\" \\\"");
-            Console.WriteLine(" /______\\\"");
+            Console.WriteLine("Hur många dagar vill du hyra bilen? ");
+            int dagar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hur många kilometer ska du köra? ");
+            int km = int.Parse(Console.ReadLine());
+
+            int kostnad = 300 + km * 1 + (dagar - 1) * 500;
+
+            Console.WriteLine($"Totala hyran blir {kostnad} kr.");
+
+            Console.ReadKey();
         }
     }
 }
